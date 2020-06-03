67f89dc770fe80d09288b815a13284dd7348ab29
STEdirisuriya <supunedirisuriya2002@gmail.com>
2020-06-03 10:18:13 +0530

    convereted webpage to jekyll

--- static/JS/tasks.js
+++ static/JS/tasks.js
@@ -32,7 +32,7 @@ function switchTheme(e) {
         localStorage.setItem('theme', 'dark');
         textChangeDark();
     }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
+    else { document.documentElement.setAttribute('data-theme', 'light');
           localStorage.setItem('theme', 'light');
           textChangeLight();
     }    
