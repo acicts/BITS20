edd64e5b593dd5290afac1c4687cea9fb3ecdc31
chamindujs <chamindujs@gmail.com>
2020-06-04 22:38:39 +0530

    responsive design

--- _site/static/JS/index.js
+++
@@ -1,6 +0,0 @@
-// $(function() {
-//     $('a[href*=#]').on('click', function(e) {
-//       e.preventDefault();
-//       $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-//     });
-//   });
